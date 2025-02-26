4a8e30b9c883eeb888bd1757dd775d4716739533
Kevin Franklin <kevin@franklindesign.io>
2025-02-25 20:23:06 -0500

    chore: Add Product class, extending DatabaseObject

---
+++ private/classes/Product.class.php
@@ -0,0 +1,76 @@
+<?php
+
+class Product extends DatabaseObject
+{
+  static protected $table_name = "product";
+  static protected $primary_key = "product_id";
+
+  static protected $db_columns = [
+    'product_id',
+    'product_name',
+    'product_description',
+    'vendor_id',
+    'product_image_url',
+    'date_added',
+    'is_active',
+    'category_id'
+  ];
+
+  public $product_id;
+  public $product_name;
+  public $product_description;
+  public $vendor_id;
+  public $product_image_url;
+  public $date_added;
+  public $is_active;
+  public $category_id;
+
+  public function __construct($args = [])
+  {
+    $this->product_name = $args['product_name'] ?? '';
+    $this->product_description = $args['product_description'] ?? '';
+    $this->vendor_id = $args['vendor_id'] ?? '';
+    $this->product_image_url = $args['product_image_url'] ?? '';
+    $this->date_added = $args['date_added'] ?? date('Y-m-d H:i:s');
+    $this->is_active = $args['is_active'] ?? 1;
+    $this->category_id = $args['category_id'] ?? '';
+  }
+
+  /**
+   * Get all products for a specific vendor.
+   *
+   * @param int $vendor_id The vendor's user ID.
+   * @return array List of products.
+   */
+
+  static public function find_by_vendor($vendor_id)
+  {
+    $sql = "SELECT * FROM " . static::$table_name . " WHERE vendor_id = ?";
+    return static::find_by_sql($sql, [$vendor_id]);
+  }
+
+  /**
+   * Validate product input before saving.
+   *
+   * @return array List of validation errors.
+   */
+
+  protected function validate()
+  {
+    $this->errors = [];
+
+    if (empty($this->product_name)) {
+      $this->errors[] = "Product name cannot be blank.";
+    }
+
+    if (empty($this->product_description)) {
+      $this->errors[] = "Product description cannot be blank.";
+    }
+
+    if (!is_numeric($this->vendor_id)) {
+      $this->errors[] = "Invalid vendor ID.";
+    }
+
+    return $this->errors;
+  }
+}
